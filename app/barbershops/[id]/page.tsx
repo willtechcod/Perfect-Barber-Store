47fe7fde0819e9bf1d9e748b64f075ce4d2a15f5
willtechcod <dev.pereira2019@gmail.com>
2024-02-15 03:44:31 -0300

    feat: create serviceItem and stilized components

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/app/_lib/prisma";
 import BarbershopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
 
 
 interface BarbershopDetailsProps {
@@ -16,6 +17,9 @@ const BarbershopDetailsPage = async ({params}: BarbershopDetailsProps) => {
     const barbershop = await db.barbershop.findUnique({
         where: {
             id: params.id,
+        },
+        include: {
+            services: true,
         }
     });
 
@@ -23,7 +27,15 @@ const BarbershopDetailsPage = async ({params}: BarbershopDetailsProps) => {
         return null
     }
     
-    return <BarbershopInfo barbershop={barbershop}/>
+    return <div>
+        <BarbershopInfo barbershop={barbershop}/>
+
+        <div className="px-5 flex flex-col gap-4 py-6">
+            {barbershop.services.map(service => (
+                <ServiceItem key={service.id} service={service} />
+            ))}
+        </div>
+    </div>
 }
  
 export default BarbershopDetailsPage;
