578700920be790e2b4c7f9986bfcf861879741d8
willtechcod <dev.pereira2019@gmail.com>
2024-02-16 03:08:54 -0300

    feat: auth

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,11 @@
+"use client"
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 
 const Header = () => {
+   
     return ( 
         <Card>
             <CardContent className="p-5 justify-between items-center flex flex-row">
