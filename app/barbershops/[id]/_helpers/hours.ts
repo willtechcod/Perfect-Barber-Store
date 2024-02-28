c1a51699badbb0023ee1a6d52b4e2b1129ee9f80
willtechcod <dev.pereira2019@gmail.com>
2024-02-28 10:29:58 -0300

    feat: add helpers

---
+++ app/barbershops/[id]/_helpers/hours.ts
@@ -0,0 +1,17 @@
+import { setHours, setMinutes, format, addMinutes } from 'date-fns';
+
+export function generateDayTimeList(date: Date): string[] {
+    const startTime = setMinutes(setHours(date, 9), 0); // set start time to 09:00
+    const endTime = setMinutes(setHours(date, 18), 0); // Set end time to 18:00
+    const interval = 60; // interval in minutes
+    const timeList: string[] = [];
+
+    let currentTime = startTime;
+
+    while (currentTime <= endTime) {
+        timeList.push(format(currentTime, "HH:mm"));
+        currentTime = addMinutes(currentTime, interval);
+    }
+
+    return timeList;
+}
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -35,7 +35,7 @@ const BarbershopDetailsPage = async ({params}: BarbershopDetailsProps) => {
 
         <div className="px-5 flex flex-col gap-4 py-6">
             {barbershop.services.map(service => (
-                <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user} />
+                <ServiceItem key={service.id} barbershop={barbershop} service={service} isAuthenticated={!!session?.user} />
             ))}
         </div>
     </div>
