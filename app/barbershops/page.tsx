ed8fd157dc7490ac46bdea04154f611f32834214
willtechcod <dev.pereira2019@gmail.com>
2024-05-31 20:37:24 -0300

    feat: ensure user search barbershops

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -1,6 +1,8 @@
+import { redirect } from "next/navigation";
 import BarbershopItem from "../(home)/_components/barbershop-item";
 import Header from "../_components/header";
 import { db } from "../_lib/prisma";
+import Search from "../(home)/_components/search";
 
 interface BarbershopsPageProps {
   searchParams: {
@@ -9,6 +11,10 @@ interface BarbershopsPageProps {
 }
 
 const BarbershopPage = async ({ searchParams }: BarbershopsPageProps) => {
+  if (!searchParams.search) {
+    return redirect("/");
+  }
+
   const barbershops = await db.barbershop.findMany({
     where: {
       name: {
@@ -21,11 +27,17 @@ const BarbershopPage = async ({ searchParams }: BarbershopsPageProps) => {
   return (
     <>
       <Header />
-      <div className="px-5 py-6">
+
+      <div className="px-5 py-6 flex flex-col gap-6">
+        <Search
+          defaultValues={{
+            search: searchParams.search,
+          }}
+        />
         <h1 className="text-gray-400 font-bold text-xs uppercase">
           Resultados para &quot;{searchParams.search}&quot;
         </h1>
-        <div className="grid grid-cols-2 mt-3 gap-4">
+        <div className="grid grid-cols-2 gap-4">
           {barbershops.map((barbershop) => (
             <div key={barbershop.id} className="w-full">
               <BarbershopItem barbershop={barbershop} />
